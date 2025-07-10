1293134b981f854fa5b81a66837eef110aed1d83
tor-wei <gohtorwei@gmail.com>
2025-07-10 20:30:25 +0800

    Fix tflint

--- main.tf
+++ main.tf
@@ -3,10 +3,19 @@ provider "aws" {
 }
 
 terraform {
+  required_version = ">= 1.0" 
+  
   backend "s3" {
     bucket = "sctp-ce10-tfstate"
     key    = "torwei-ce10-3-2.tfstate"
     region = "ap-southeast-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
   }
 }
 
